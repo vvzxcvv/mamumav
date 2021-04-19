1e04be149627e67a9149b88f80d1f67b12a87cd5
vvzxcvv <rentonyt@gmail.com>
2021-04-19 08:10:11 +0500

    loancalc

--- alert.js
+++ alert.js
@@ -1,22 +1,30 @@
+"use strict";
+
+//   let user = prompt("name");
+//   if (user == "admin") {
+//     let pass = prompt("pass");
+//     if (pass == "zzz") {
+//       alert("da");
+//     } else if ((pass = null || pass == "")) {
+//       alert("hz)");
+//     } else {
+//       alert("ne)");
+//     }
+//   } else if (user == null || user == "") {
+//     alert("hz kto");
+//   } else {
+//     alert("nezn)");
+//   }
+// }
+
 function clickEvent() {
-'use strict';
+  const value = prompt("skolko deneg");
+  const monthNumber = prompt("kakoi mesyatc");
 
-let user = prompt ("name");
-if (user == "admin") {
+  const loanCalc = function (value) {
+    let definedMonth = value * 0.9 ** monthNumber;
+    return definedMonth;
+  };
 
-    let pass = prompt ("pass")
-    if (pass == "zzz") {
-        alert ("da");
-    } else if (pass = null || pass == '') {
-        alert ("hz)");
-    } else {
-        alert ("ne)");
-    }
-
-} else if (user == null || user == '') {
-    alert ("hz kto")
-} else {
-    alert ("nezn)")
-}
-
+  console.log(`v ${monthNumber} mesyace ti ewe doljen ${loanCalc(value)}`);
 }
