a5ec8c1376cc0871a65197fca374e2142531645b
vvzxcvv <rentonyt@gmail.com>
2021-04-20 05:18:42 +0500

    the LEGENDARNIY calcul v2 was repaired

--- alert.js
+++ alert.js
@@ -28,3 +28,36 @@ function loanCalculator() {
 
   alert(`v ${monthNumber} mesyace ti ewe doljen ${loanCalc(value)}rub`);
 }
+
+function calcul_21() {
+  alert("всем привет это калькуль 2.1!!!!!!))");
+  const vibor = prompt("Выберите действие: +  -  *  /");
+  const a = Number(prompt("Первое число: "));
+  const b = Number(prompt("Второе число: "));
+
+  switch (vibor) {
+    case "+":
+      const resultatPlus = a + b;
+      alert(resultatPlus);
+      break;
+
+    case "-":
+      const resultatMinus = a - b;
+      alert(resultatMinus);
+      break;
+
+    case "*":
+      const resultatMult = a * b;
+      alert(resultatMult);
+      break;
+
+    case "/":
+      const resultatDecl = a / b;
+      alert(resultatDecl);
+      break;
+
+    default:
+      alert('блять ну сказали же выбрать из " +  -  *  / " что сложного то сука');
+      break;
+  }
+}
